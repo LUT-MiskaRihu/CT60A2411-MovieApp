e0e66e4df9eaeaacdaa300f6e19d92d2aec38864
Miska Rihu <miska.rihu@student.lut.fi>
2022-04-19 18:52:29 +0300

    Added methods: - getNodesByTagName(...) - getTextContent(...) - getTheatres() - getShows()

--- app/src/main/java/com/example/test/XMLReader.java
+++ app/src/main/java/com/example/test/XMLReader.java
@@ -1,12 +1,13 @@
 package com.example.test;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.ArrayList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -17,11 +18,11 @@ public class XMLReader {
     private final String scheduleURL = "https://www.finnkino.fi/xml/Schedule";
     private final String theatreTag = "TheatreArea";
     private final String showTag = "Show";
-    private XMLReader instance = null;
+    private static XMLReader instance = null;
 
     public XMLReader() {    }
 
-    public XMLReader getInstance() {
+    public static XMLReader getInstance() {
         if (instance == null) {
             instance = new XMLReader();
         }
@@ -29,7 +30,7 @@ public class XMLReader {
     }
 
     /**
-     * Takes XML url and tag name as parameters, returns nodelist of found elements.
+     * Takes XML url and tag name as parameters, returns NodeList of found elements.
      * @param url XML url
      * @param tagName XML tag name
      * @return NodeList of found nodes
@@ -55,12 +56,55 @@ public class XMLReader {
         return nodes;
     }
 
-    public List<Theatre> getTheatres() {
-        List<Theatre> theatres = new List<Theatre>();
+    private String getTextContent(Element e, String tagName) {
+        return e.getElementsByTagName(tagName).item(0).getTextContent().toString();
+    }
+
+    /**
+     *
+     * @return
+     */
+    public ArrayList<Theatre> getTheatres() {
+        ArrayList<Theatre> theatres = new ArrayList<Theatre>();
         NodeList theatreNodes = getNodesByTagName(theatresURL, theatreTag);
         for (int i=0; i<theatreNodes.getLength(); i++) {
-
+            Node theatreNode = theatreNodes.item(i);
+            if (theatreNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element theatreElement = (Element) theatreNode;
+                Theatre theatre = new Theatre();
+                theatre.setID(Integer.parseInt(theatreElement.getElementsByTagName("ID").item(0).getTextContent()));
+                theatre.setName(theatreElement.getElementsByTagName("Name").item(0).getTextContent());
+                theatres.add(theatre);
+            }
         }
         return theatres;
+    }
+
+    public ArrayList<Show> getShows() {
+        ArrayList<Show> shows = new ArrayList<Show>();
+        NodeList showNodes = getNodesByTagName(scheduleURL, showTag);
+        for (int i=0; i<showNodes.getLength(); i++) {
+            Node showNode = showNodes.item(i);
+            if (showNode.getNodeType() == Element.ELEMENT_NODE) {
+                Element showElement = (Element) showNode;
+                Show show = new Show();
+                show.setID(getTextContent(showElement, "ID"));
+                show.setDateTime(Show.START_DATE_TIME, getTextContent(showElement, "dttmShowStart"));
+                show.setDateTime(Show.END_DATE_TIME, getTextContent(showElement, "dttmShowEnd"));
+                show.setEventID(getTextContent(showElement, "EventID"));
+                show.setTitle(getTextContent(showElement, "Title"));
+                show.setOriginalTitle(getTextContent(showElement, "OriginalTitle"));
+                show.setProductionYear(getTextContent(showElement, "ProductionYear"));
+                show.setLength(getTextContent(showElement, "LengthInMinutes"));
+                show.setDateTime(Show.LOCAL_RELEASE, getTextContent(showElement, "dtLocalRelease"));
+                show.setRating(getTextContent(showElement, "Rating"));
+                show.setEventType(getTextContent(showElement, "EventType"));
+                show.setGenres(getTextContent(showElement, "Genres"));
+                show.setLocationID(getTextContent(showElement, "TheatreID"));
+                show.setPresentationMethodAndLanguage(getTextContent(showElement, "PresentationMethodAndLanguage"));
+                shows.add(show);
+            }
+        }
+        return shows;
     }
 }
