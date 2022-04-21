923752f7972466d71551bdc621715eaf76cea024
Miska Rihu <miska.rihu@student.lut.fi>
2022-04-21 17:15:54 +0300

    Added:  - ArrayList that holds the shows of a theatre  - methods for adding shows to these lists

--- app/src/main/java/com/example/movieapp/Theatre.java
+++ app/src/main/java/com/example/movieapp/Theatre.java
@@ -1,17 +1,23 @@
 package com.example.movieapp;
 
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+
 public class Theatre {
     private int id;
     private String name;
+    private ArrayList<Show> shows = new ArrayList<>();
 
     public Theatre() {
         id = 0;
         name = null;
     }
 
-    public Theatre(int id, String name) {
+    public Theatre(int id, String name, ArrayList<Show> shows) {
         this.id = id;
         this.name = name;
+        addShows(shows);
     }
 
     public int getID() {
@@ -30,6 +36,25 @@ public class Theatre {
         this.name = name;
     }
 
+    public Show addShow(Show show) {
+        shows.add(show);
+        return show;
+    }
+
+    public void addShows(@NonNull ArrayList<Show> shows) {
+        for (int i=0; i<shows.size(); i++) {
+            Show show = shows.get(i);
+            if (!this.shows.contains(show)) {
+                this.shows.add(show);
+            }
+        }
+    }
+
+    public ArrayList<Show> getShows() {
+        return shows;
+    }
+
+    @NonNull
     @Override
     public String toString() {
         return this.name;
