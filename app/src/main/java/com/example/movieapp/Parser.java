147e1035f645629b3e00832ba3fe1bbae6277343
LUT-MiskaRihu <Miska.Rihu@student.lut.fi>
2022-04-28 22:46:08 +0300

    Added:  - DATE_FORMAT (SimpleDateFormat, dd.MM.yyyy)  - TIME_FORMAT (SimpleDateFormat, HH:mm)  - parseDate method (returns Date object)  - parseTime method (returns Date object)

--- app/src/main/java/com/example/movieapp/Parser.java
+++ app/src/main/java/com/example/movieapp/Parser.java
@@ -5,17 +5,45 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+@SuppressLint("SimpleDateFormat")
 public class Parser {
-    @SuppressLint("SimpleDateFormat")
-    public static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+    public static final SimpleDateFormat DATE_TIME_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+    public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd.MM.yyyy");
+    public static final SimpleDateFormat TIME_FORMAT = new SimpleDateFormat("HH:mm");
 
     public static Date parseDateTime(String dateTime) {
         Date date = null;
+
         try {
-            date = SIMPLE_DATE_FORMAT.parse(dateTime);
+            date = DATE_TIME_FORMAT.parse(dateTime);
         } catch (ParseException e) {
             e.printStackTrace();
         }
+
         return date;
+    }
+
+    public static Date parseDate(String sDate) {
+        Date date = null;
+
+        try {
+            date = DATE_FORMAT.parse(sDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return date;
+    }
+
+    public static Date parseTime(String sTime) {
+        Date time = null;
+
+        try {
+            time = TIME_FORMAT.parse(sTime);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return time;
     }
 }
