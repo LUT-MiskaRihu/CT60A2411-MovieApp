69c817517ee333776574cafda15f4413f62fd4bb
LUT-MiskaRihu <Miska.Rihu@student.lut.fi>
2022-05-02 01:03:21 +0300

    Modified:  - replaced anonymous new view methods with lambda  - code cleanup

--- app/src/main/java/com/example/movieapp/SearchActivity.java
+++ app/src/main/java/com/example/movieapp/SearchActivity.java
@@ -1,24 +1,19 @@
 package com.example.movieapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
-import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TimePicker;
 import android.widget.Toast;
-
 import java.util.Calendar;
 import java.util.Date;
 
@@ -33,15 +28,13 @@ public class SearchActivity extends AppCompatActivity {
     private Button btnClearDate;
     private Button btnClearTimeMin;
     private Button btnClearTimeMax;
-    private Button btnSearch;
-    private TextWatcher textWatcher;
 
     private Filter filter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_search);
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
@@ -64,43 +57,40 @@ public class SearchActivity extends AppCompatActivity {
         filter = Filter.getInstance();
 
         // Set onClick actions
-        View.OnClickListener onClickListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Pick date
-                if (view == etDate) {
-                    pickDate();
-                }
+        View.OnClickListener onClickListener = view -> {
+            // Pick date
+            if (view == etDate) {
+                pickDate();
+            }
 
-                // Pick time min
-                else if (view == etTimeMin) {
-                    pickTime(etTimeMin);
-                }
+            // Pick time min
+            else if (view == etTimeMin) {
+                pickTime(etTimeMin);
+            }
 
-                // Pick time max
-                else if (view == etTimeMax) {
-                    pickTime(etTimeMax);
-                }
+            // Pick time max
+            else if (view == etTimeMax) {
+                pickTime(etTimeMax);
+            }
 
-                // Clear title
-                else if (view == btnClearTitle) {
-                    etTitle.setText(null);
-                }
+            // Clear title
+            else if (view == btnClearTitle) {
+                etTitle.setText(null);
+            }
 
-                // Clear date
-                else if (view == btnClearDate) {
-                    etDate.setText(null);
-                }
+            // Clear date
+            else if (view == btnClearDate) {
+                etDate.setText(null);
+            }
 
-                // Clear time min
-                else if (view == btnClearTimeMin) {
-                    etTimeMin.setText(null);
-                }
+            // Clear time min
+            else if (view == btnClearTimeMin) {
+                etTimeMin.setText(null);
+            }
 
-                // Clear time max
-                else if (view == btnClearTimeMax) {
-                    etTimeMax.setText(null);
-                }
+            // Clear time max
+            else if (view == btnClearTimeMax) {
+                etTimeMax.setText(null);
             }
         };
 
@@ -111,24 +101,6 @@ public class SearchActivity extends AppCompatActivity {
         btnClearDate.setOnClickListener(onClickListener);
         btnClearTimeMin.setOnClickListener(onClickListener);
         btnClearTimeMax.setOnClickListener(onClickListener);
-
-//        etTitle.addTextChangedListener(new TextWatcher() {
-//            @Override
-//            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-//
-//            }
-//
-//            @Override
-//            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-//
-//            }
-//
-//            @Override
-//            public void afterTextChanged(Editable editable) {
-//                filter.setTitle(etTitle.getText().toString());
-//            }
-//        });
-
     }
 
     // Makes it impossible to go back to the splash screen
@@ -139,15 +111,10 @@ public class SearchActivity extends AppCompatActivity {
      * This function shows the DatePickerDialog when the user clicks on the date field,
      * and sets the date params in filter object (year, month, and day of month)
      */
+    @SuppressLint("DefaultLocale")
     public void pickDate() {
         DatePickerDialog.OnDateSetListener onDateSetListener;
-        onDateSetListener = new DatePickerDialog.OnDateSetListener() {
-            @SuppressLint("DefaultLocale")
-            @Override
-            public void onDateSet(DatePicker datePicker, int iYear, int iMonth, int iDay) {
-                etDate.setText(String.format("%02d.%02d.%04d", iDay, iMonth + 1, iYear));
-            }
-        };
+        onDateSetListener = (datePicker, iYear, iMonth, iDay) -> etDate.setText(String.format("%02d.%02d.%04d", iDay, iMonth + 1, iYear));
 
         DatePickerDialog datePickerDialog;
         datePickerDialog = new DatePickerDialog(
@@ -166,20 +133,10 @@ public class SearchActivity extends AppCompatActivity {
      * time fields, and sets the filters attributes accordingly.
      * @param etTimeField text field to edit
      */
+    @SuppressLint("DefaultLocale")
     public void pickTime(EditText etTimeField) {
         TimePickerDialog.OnTimeSetListener onTimeSetListener;
-        onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
-            @SuppressLint("DefaultLocale")
-            @Override
-            public void onTimeSet(TimePicker timePicker, int iHour, int iMinute) {
-//                if (etTimeField.equals(etTimeMin)) {
-//                    filter.setTimeFor(Filter.START_DT_MIN, iHour, iMinute);
-//                } else if (etTimeField.equals(etTimeMax)) {
-//                    filter.setTimeFor(Filter.START_DT_MAX, iHour, iMinute);
-//                }
-                etTimeField.setText(String.format("%2d:%02d", iHour, iMinute));
-            }
-        };
+        onTimeSetListener = (timePicker, iHour, iMinute) -> etTimeField.setText(String.format("%2d:%02d", iHour, iMinute));
 
         TimePickerDialog timePickerDialog = new TimePickerDialog(
                 SearchActivity.this,
@@ -191,13 +148,6 @@ public class SearchActivity extends AppCompatActivity {
 
         timePickerDialog.show();
     }
-
-//    /**
-//     *
-//     */
-//    public void pickTheatre() {
-//        filter.setLocationID(spnTheatres.getSelectedItem().toString());
-//    }
 
     public void search(View view) {
         setFilterTheatre();
@@ -214,12 +164,6 @@ public class SearchActivity extends AppCompatActivity {
         if (bNoErrors) {
             loadDebugActivity(view);
         }
-    }
-
-    public void showFilter() {
-        System.out.println("#### SearchActivity.showFilter() ##############################");
-        System.out.println(filter.toString());
-        System.out.println("#############################################################");
     }
 
     public void loadDebugActivity(View view) {
@@ -240,7 +184,7 @@ public class SearchActivity extends AppCompatActivity {
         String sTitle = etTitle.getText().toString();
 
         if (sTitle.equals("")) {
-            Log.w(sClassTag + "." + sMethodTag, "Input string sTitle is empty.");
+            Log.w(sClassTag + "." + sMethodTag, "Input string sTitle is empty, using default value.");
             filter.clearTitle();
         } else {
             Log.i(sClassTag + "." + sMethodTag, "Setting filter's title to '" + sTitle + "'.");
@@ -253,7 +197,7 @@ public class SearchActivity extends AppCompatActivity {
         String sDate = etDate.getText().toString();
 
         if (sDate.equals("")) {
-            Log.w(sClassTag + "." + sMethodTag, "Input string sDate is empty.");
+            Log.w(sClassTag + "." + sMethodTag, "Input string sDate is empty, using default value.");
             filter.clearMinStartDate();
         } else {
             Log.i(sClassTag + "." + sMethodTag, "Setting filter's date to '" + sDate + "'.");
@@ -268,7 +212,7 @@ public class SearchActivity extends AppCompatActivity {
         boolean success = true;
 
         if (sTimeMin.equals("")) {
-            Log.w(sClassTag + "." + sMethodTag, "Input string sTimeMin is empty.");
+            Log.w(sClassTag + "." + sMethodTag, "Input string sTimeMin is empty, using default value.");
             filter.clearMinStartTime();
         } else {
             String sDate = etDate.getText().toString();
