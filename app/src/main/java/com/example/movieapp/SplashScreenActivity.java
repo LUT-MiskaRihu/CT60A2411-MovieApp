e3f9d8b67a3e4cc9ddb16a0deb51ca489ebbaeb3
LUT-MiskaRihu <Miska.Rihu@student.lut.fi>
2022-05-01 16:03:40 +0300

    Modified:  - cleaned imports

--- app/src/main/java/com/example/movieapp/SplashScreenActivity.java
+++ app/src/main/java/com/example/movieapp/SplashScreenActivity.java
@@ -1,9 +1,8 @@
 package com.example.movieapp;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
