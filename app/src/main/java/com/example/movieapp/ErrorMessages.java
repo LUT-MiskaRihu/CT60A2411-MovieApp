3f5ed74eab66ae34261c2432ef46431af753dd7a
LUT-MiskaRihu <Miska.Rihu@student.lut.fi>
2022-05-02 13:12:56 +0300

    Added:  - invalid input messages to used in SearchActivity in method search.

--- app/src/main/java/com/example/movieapp/ErrorMessages.java
+++ app/src/main/java/com/example/movieapp/ErrorMessages.java
@@ -4,4 +4,8 @@ public class ErrorMessages {
     public static final String ERR_XML_READER_INVALID_CALENDAR_CODE = "Invalid calendar code: ";
     public static final String ERR_FILTER_CALENDAR_NOT_FOUND = "Specified calendar not found: ";
     public static final String ERR_FILTER_FIELD_NOT_FOUND = "Specified field not found: ";
+
+    public static final String ERR_INVALID_DATE_ENTERED = "Tarkista kenttä \"päivämäärä\". (Esim. onko päivämäärä jo mennyt.)";
+    public static final String ERR_INVALID_MIN_START_TIME_ENTERED = "Tarkista kenttä \"alkaa aikaisintaan\". (Esim. onko aika jo mennyt.)";
+    public static final String ERR_INVALID_MAX_START_TIME_ENTERED = "Tarkista kenttä \"alkaa viimeistään\". (Esim. onko aika jo mennyt.)";
 }
