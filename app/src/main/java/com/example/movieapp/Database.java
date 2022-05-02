abdb103bbd932e350eaa5b8ea84ec103b7197d44
LUT-MiskaRihu <Miska.Rihu@student.lut.fi>
2022-05-02 13:10:08 +0300

    Added:  - method to load all shows to an ArrayList  - method to sort all shows in ascending order by their start date time
    Code cleanup:
     - removed unnecessary lines
     - renamed variables and methods

--- app/src/main/java/com/example/movieapp/Database.java
+++ app/src/main/java/com/example/movieapp/Database.java
@@ -1,22 +1,20 @@
 package com.example.movieapp;
 
 import android.util.Log;
-
+import androidx.annotation.NonNull;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Database {
-    private static ArrayList<Theatre> theatres = null;
-    private static ArrayList<Show> shows = null;
+    private static ArrayList<Theatre> alTheatres = null;
+    private static ArrayList<Show> alAllShows = null;
     private static Database instance = null;
 
     private Database() {
         Log.i("Database.Database", "Started creating a new database.");
-        getTheatreInformation();
-        getShowInformation();
-
-        // Store all shows for future
-        // these will be saved to a local database in the future (json, xml, or csv)
-        shows = getTheatre("Valitse alue/teatteri").getShows();
+        loadTheatreInformation();
+        loadShowInformation();
         Log.i("Database.Database","Finished creating a new database.");
     }
     
@@ -27,46 +25,66 @@ public class Database {
         return instance;
     }
 
-    private static void getTheatreInformation() {
+    private static void loadTheatreInformation() {
         // Get theatres and IDs
-        if (theatres == null) {
+        if (alTheatres == null) {
             Log.i("Database.getTheatreInformation","Started fetching theatres/areas.");
-            theatres = XMLReader.getOnlineTheatres();
+            alTheatres = XMLReader.getOnlineTheatres();
         }
-        for (Theatre t : theatres) {
+        for (Theatre t : alTheatres) {
             System.out.println("\t\t" + t.toString());
         }
-        Log.i("Database.getTheatreInformation","Finished fetching theatres/areas. Found " + theatres.size() + " theatres/areas.");
+        Log.i("Database.getTheatreInformation","Finished fetching theatres/areas. Found " + alTheatres.size() + " theatres/areas.");
     }
 
-    private static void getShowInformation() {
-        // Get movies for each theater
-        for (Theatre theatre : theatres) {
+    private static void loadShowInformation() {
+        // Get shows for each theater
+        alAllShows = new ArrayList<>();
+
+        for (Theatre theatre : alTheatres.subList(1, alTheatres.size() - 1)) {
             int iTheatreID = theatre.getID();
             Log.i("Database.getShowInformation","Started fetching shows for theatre " + iTheatreID + ".");
+
             if (theatre.getShows().size() == 0) {
                 Log.i("Database.getShowInformation","Downloading data...");
                 theatre.addShows(XMLReader.getOnlineShowsAt(iTheatreID));
-                if (theatre.getShows() != null) {
+                ArrayList<Show> alShows = theatre.getShows();
+
+                if (alShows.size() != 0) {
+                    alAllShows.addAll(alShows); // add each theatre's shows to the list containing all shows.
                     Log.i("Database.getShowInformation","Finished fetching shows for theatre " + iTheatreID + ". Found " + theatre.getShows().size() + " shows.");
                 }
             }
         }
+
+        sortAllShowsByStarTime();
+    }
+
+    private static void sortAllShowsByStarTime() {
+        Collections.sort(alAllShows, new Comparator<Show>() {
+            @Override
+            public int compare(Show show1, Show show2) {
+                return Long.compare(show1.getStartDateTime().getTime(), show2.getStartDateTime().getTime());
+            }
+        });
     }
 
     public static ArrayList<Theatre> getTheatres() {
-        for (Theatre t : theatres) {
+        for (Theatre t : alTheatres) {
             Log.d("Database.getTheatres",t.toString());
         }
-        Log.i("Database.getTheatres","Returned ArrayList, " + theatres.size() + " theatre(s).");
-        return theatres;
+        Log.i("Database.getTheatres","Returned ArrayList, " + alTheatres.size() + " theatre(s).");
+        return alTheatres;
     }
 
+    @NonNull
     public static ArrayList<Show> getShowsAt(int iTheatreID) {
-        ArrayList<Show> alShows = getTheatre(iTheatreID).getShows();
+        ArrayList<Show> alShows;
 
-        for (Show show : alShows) {
-            Log.d("Database.getShowsAt",show.toString());
+        if (iTheatreID == Filter.ANY_LOCATION) {
+            alShows = alAllShows;
+        } else {
+            alShows = getTheatre(iTheatreID).getShows();
         }
 
         Log.i("Database.getShowsAtTheatres","Returned ArrayList, " + alShows.size() + " show(s).");
@@ -75,7 +93,7 @@ public class Database {
 
     public static Theatre getTheatre(int id) {
         Theatre theatre = null;
-        for (Theatre t : theatres) {
+        for (Theatre t : alTheatres) {
             if (t.getID() == id) {
                 theatre = t;
             }
@@ -85,7 +103,7 @@ public class Database {
 
     public static Theatre getTheatre(String name) {
         Theatre theatre = null;
-        for (Theatre t : theatres) {
+        for (Theatre t : alTheatres) {
             if (t.getName().equals(name)) {
                 theatre = t;
             }
