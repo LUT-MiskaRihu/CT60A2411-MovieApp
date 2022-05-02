946161362f43a3a133f944148a6ed9cc4b78362e
LUT-MiskaRihu <Miska.Rihu@student.lut.fi>
2022-05-02 13:24:48 +0300

    Changed version number to A20220502-1.

--- app/build/generated/source/buildConfig/debug/com/example/movieapp/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/example/movieapp/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.example.movieapp";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 20220421;
-  public static final String VERSION_NAME = "A20220422-1";
+  public static final int VERSION_CODE = 20220502;
+  public static final String VERSION_NAME = "A20220502-1";
 }
